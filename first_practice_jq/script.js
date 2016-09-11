522c321801ed1277b10b9803cb70cc691dcf558b
Atsuky <a.mitsuyori.15@gmail.com>
2016-09-12 00:23:16 +0900

    文字クリックしたら変わるやつ

---
+++ first_practice_jq/script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+	$(function(){
+    
+	$("#jQueryBox").css("display", "none");
+ 
+    
+	$("#jQueryPush").click(function(){
+
+	$("#jQueryBox").toggle();
+    
+    });
+});
+
+</script>
