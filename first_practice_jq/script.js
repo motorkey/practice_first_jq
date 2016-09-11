1b902b124dde9870a5fdfcbad01b24377cbf898a
motorkey <auagcumitsumo@gmail.com>
2016-09-12 00:48:08 +0900

    Update script.js
    <script>は"html"タグやからjsファイルに書いたらあかん。
    あと不要な }があったから消した。

--- first_practice_jq/script.js
+++ first_practice_jq/script.js
@@ -1,14 +1,5 @@
-<script type="text/javascript">
-	$(function(){
-    
-	$("#jQueryBox").css("display", "none");
- 
-    
-	$("#jQueryPush").click(function(){
-
-	$("#jQueryBox").toggle();
-    
-    });
+$(function(){
+  $("#jQueryBox").css("display", "none");
+  $("#jQueryPush").click(function(){
+  $("#jQueryBox").toggle();
 });
-
-</script>
